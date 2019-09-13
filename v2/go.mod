dd0086bf9a3fb54083cbeffbe97f1995c76d4f98
chuckha <ha.chuck@gmail.com>
2019-09-13 19:32:28 -0400

    v2
    Signed-off-by: chuckha <ha.chuck@gmail.com>

--- v2/go.mod
+++ v2/go.mod
@@ -2,4 +2,8 @@ module github.com/chuckha/kubeyaml/v2
 
 go 1.12
 
-require cuelang.org/go v0.0.8
+require (
+	cuelang.org/go v0.0.8
+	github.com/chuckha/cluster-api v0.0.0-20190901142157-b13118604994 // indirect
+	k8s.io/api v0.0.0-20190831074750-7364b6bdad65 // indirect
+)
--- v2/main.go
+++ v2/main.go
@@ -2,16 +2,40 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 
 	"cuelang.org/go/cue"
-	"cuelang.org/go/encoding/yaml"
+	"cuelang.org/go/encoding/gocode"
 )
 
 func main() {
+	// filepath.Walk("pkg", func(path string, info os.FileInfo, err error) error {
+	// 	if strings.HasSuffix("types_go_gen.cue")
+	// })
+
+	f, _ := os.Open("./pkg/k8s.io/api/core/v1/types_go_gen.cue")
 	var r cue.Runtime
-	c, err := yaml.Decode(&r, "testdata/pod.yaml", "test")
+
+	instance, err := r.Compile("testdata/pod.cue", f)
 	if err != nil {
-		panic(err)
+		fmt.Println("failed to compile", err)
+		return
 	}
-	fmt.Println(c)
+
+	b, err := gocode.Generate("k8s.io/api/core/v1", instance, nil)
+	if err != nil {
+		fmt.Println("failed to generate", err)
+		return
+	}
+	if err := ioutil.WriteFile("cue_gen.go", b, 0644); err != nil {
+		fmt.Println("failed to write file", err)
+		return
+	}
+
+	// c, err := yaml.Decode(&r, "testdata/pod.yaml", "test")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(c)
 }
